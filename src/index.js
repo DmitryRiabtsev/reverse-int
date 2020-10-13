8af889a2fc6548c4355ffb3cf384debc126452f7
Dmitry <dim4ik7558118@gmail.com>
2020-10-13 22:42:10 +0300

    test: test done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
+	let str = String(n);
+	let strArr = str.split('');
+	for(let i =0; i<strArr.length; i++){
+		if(strArr[i]=='-'){
+			strArr.splice(0,1);
+		}
+		if(strArr[strArr.length-1]=='0'){
+			strArr.splice(strArr.length-1,strArr.length-1);
+		}
+	}
+return parseInt(strArr.reverse().join(''));
   
 }
