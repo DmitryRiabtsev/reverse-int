042d731de752ad324c89484e6f463285e4cd322e
Dmitry <dim4ik7558118@gmail.com>
2021-03-01 22:46:17 +0300

    feat:add new function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 module.exports = function reverse (n) {
-	let str = String(n);
-	let strArr = str.split('');
-	for(let i =0; i<strArr.length; i++){
-		if(strArr[i]=='-'){
-			strArr.splice(0,1);
-		}
-		if(strArr[strArr.length-1]=='0'){
-			strArr.splice(strArr.length-1,strArr.length-1);
-		}
-	}
-return parseInt(strArr.reverse().join(''));
-  
+	return Number(n.toString().split('').filter((item,index)=>{
+		if(item != '-') return item;
+		if(index == n.length && item == '0') delete item;
+	}).reverse().join(''));
 }
